dd08adff8c95b65244a9b805188074ee5efe3e69
shinseungmin-kor <shinseungmin10@gmail.com>
2021-09-09 01:45:26 +0900

    21.09.09

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
+ 
 function App() {
+  const [count, setCount] = useState(0);
+  const [counted, setCounted] = useState(0);
+
+  useEffect(() => {
+    console.log(count)
+    console.log(counted)
+  },[count, counted])
+
+  useEffect(() => {
+    console.log('first rendering')
+  }, []);
+ 
+  const increment = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setCounted(counted + 1)}>Click2</button>
     </div>
   );
 }
